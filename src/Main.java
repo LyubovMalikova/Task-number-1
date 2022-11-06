5a94bbf74ef52c8ac7558485e2ca848eb619d885
Lyubov Malikova <lu.malikova58@gmail.com>
2022-11-06 21:17:50 +0300

    Mili

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int amount = 3000;
+        int mili = 20;
+
+        System.out.println("Бонус - " + amount / mili + " миль");
+
+
+
+    }
+}
